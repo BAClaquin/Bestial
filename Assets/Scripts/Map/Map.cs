e4dbafba6563769ffccffc79bfc388e205ef58cc
Jonathan F <j_fontaine@me.com>
2020-04-15 15:36:06 +0200

    creation map de base

---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    // number of tiles on X axis
+    public int SizeX;
+    // number of tiles on Y axis
+    public int SizeY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
