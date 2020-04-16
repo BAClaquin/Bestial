a524f2ce16ddcbe8e313bbf0bdb8f24323b95273
Jonathan F <j_fontaine@me.com>
2020-04-16 17:59:50 +0200

    mouvement de 1 case

--- Assets/Scripts/Map/Tile.cs
+++ Assets/Scripts/Map/Tile.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Drawing;
 
 /// <summary>
 /// Tile is an element of a map
@@ -36,10 +37,52 @@ public class Tile : MonoBehaviour
     public int DefenseBonus;
     #endregion
 
+    #region Private Memebrs
+    /// <summary>
+    /// Sprite render properties of this tile
+    /// </summary>
+    private SpriteRenderer m_render;
+    /// <summary>
+    /// Position of the tile in the grid
+    /// </summary>
+    private Point m_gridPosition;
+    /// <summary>
+    /// Manager of the current game played
+    /// </summary>
+    private Game m_game;
+    #endregion
+
+    #region Game tags
+    /// <summary>
+    /// tagged as move possible during a game move
+    /// </summary>
+    private bool m_tagMovePossible = false;
+    #endregion
+
+    #region Public Functions
+    /// <summary>
+    ///  Sets the place of the tile in the grid position
+    /// </summary>
+    /// <param name="ai_position">Position to set tile to</param>
+    public void setGridPosition(Point ai_position) { m_gridPosition = ai_position;  }
+    /// <summary>
+    /// Get the position of this tile on the grid
+    /// </summary>
+    /// <returns>Point representing this position</returns>
+    public Point getGridPosition() { return m_gridPosition; }
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_render = GetComponent<SpriteRenderer>();
+        m_game = FindObjectOfType<Game>();
+        // check for UnityConstruction values
+        if (MovePossibleColor == null)
+        {
+            print("ERROR : No MovePossibleColor provided for object Game");
+            throw new System.NullReferenceException("MovePossibleColor is null");
+        }
     }
 
     // Update is called once per frame
@@ -47,4 +90,38 @@ public class Tile : MonoBehaviour
     {
         
     }
+
+    #region UI Functions
+    /// <summary>
+    /// Occurs when clicked on tile
+    /// </summary>
+    public void OnMouseDown()
+    {
+        m_game.onSelectedTile(this);
+    }
+
+    public void SetAvailableMove()
+    {
+        m_tagMovePossible = true;
+        m_render.color = MovePossibleColor;
+    }
+
+    /// <summary>
+    /// Resets game turn actions for this tile
+    /// </summary>
+    public void ResetTileActions()
+    {
+        m_tagMovePossible = false;
+        m_render.color = UnityEngine.Color.white;
+    }
+
+    /// <summary>
+    /// Acces to move possible tag
+    /// </summary>
+    /// <returns>the current state of the tag</returns>
+    public bool isTaggedAccessible()
+    {
+        return m_tagMovePossible;
+    }
+    #endregion
 }
