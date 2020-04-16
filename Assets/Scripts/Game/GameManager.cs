38ec98e5074a77a9936995e6cbec8e60f7ebbf15
Jonathan F <j_fontaine@me.com>
2020-04-16 16:19:12 +0200

    menage

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -7,59 +7,24 @@ using UnityEngine;
 /// </summary>
 public class GameManager : MonoBehaviour
 {
-    #region Private Members
+    #region Public Members
     /// <summary>
-    /// Current Game
+    /// Current Map Played
     /// </summary>
-    Game m_game;
-    /// <summary>
-    /// Instance of game manager
-    /// </summary>
-    GameManager m_instance;
-    #endregion
-
-    #region Private Functions
-    /// <summary>
-    /// Singleton should not be build
-    /// </summary>
-    private GameManager() { }
+    public Map CurrentMap;
     #endregion
 
     #region Public Functions
-    /// <summary>
-    /// Acces to the singleton class GameManager
-    /// </summary>
-    /// <returns></returns>
-    GameManager Instance()
-    {
-        if(m_instance == null)
-        {
-            m_instance = new GameManager();
-        }
-        return m_instance;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        m_game = new Game();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    #endregion
-
-    #region Public Functions
-    /// <summary>
-    /// Provides acces to the current game being played
-    /// </summary>
-    /// <returns></returns>
-    public Game getCurrentGame()
-    {
-        return m_game;
     }
     #endregion
 }
