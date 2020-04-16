fdc49befcbd6b27bd1b64b04c77fde57fede741e
Jonathan F <j_fontaine@me.com>
2020-04-16 15:53:06 +0200

    map structure and unit + selection

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Drawing;
+using System;
 
 public class Map : MonoBehaviour
 {
     #region Public Members
+    /// <summary>
+    /// Size of a tile
+    /// </summary>
+    public float TileSize;
     #endregion
 
     #region Private Members
@@ -14,30 +19,66 @@ public class Map : MonoBehaviour
     /// Delta to use to transforme a position to an index in our 2D map array
     /// </summary>
     PointF m_positionToIndex;
+    /// <summary>
+    /// 2D array of unique tile
+    /// </summary>
     Tile[,] m_tileMap;
+    /// <summary>
+    /// Have tiles been initialized
+    /// </summary>
+    bool m_tilesInitialized;
+    /// <summary>
+    /// Have tiles been initialized
+    /// </summary>
+    bool m_unitsInitialized;
+    /// <summary>
+    /// Liste of units in the map
+    /// </summary>
+    List<Unit> w_listOfUnit;
+    #endregion
+
+    #region Constructors
+    Map()
+    {
+        m_tilesInitialized = false;
+        m_unitsInitialized = false;
+        w_listOfUnit = new List<Unit>();
+    }
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        ListTiles();
+        // initialising tiles based on what's present on the scene
+        m_tilesInitialized = InitialiseTiles();
+        if(!m_tilesInitialized)
+        {
+            print("ERROR : tiles not correctly initialized : won't go further.");
+            return;
+        }
+        // initialise units based on what's present on the scene
+        m_unitsInitialized = InitialiseUnits();
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
-    #region Private functions
+    #region Private Functions
     /// <summary>
-    /// Init map on tiles based on tiles existing on the scene
+    ///  Init map on tiles based on tiles existing on the scene
     /// </summary>
-    private void ListTiles()
+    /// <returns>True if correctly init, false otherwise</returns>
+    private bool InitialiseTiles()
     {
         Vector3 w_min = new Vector3();
         Vector3 w_max = new Vector3();
         bool w_firstIter = true;
+        bool w_correctlyInitialized = true; // will be set to false when encoutering errors
 
         // get all tiles in the scene
         Tile[] w_tiles = FindObjectsOfType<Tile>();
@@ -81,11 +122,12 @@ public class Map : MonoBehaviour
         foreach (Tile w_tile in w_tiles)
         {
             // get index based on tile position
-            w_index = convertPositionToIndexes(w_tile.transform.position);
+            w_index = convertToGridPosition(w_tile.transform.position);
             // check if index is free
             if (m_tileMap[w_index.X, w_index.Y] != null)
             {
                 print("ERROR : tile is full for position = " + w_index);
+                w_correctlyInitialized = false;
             }
             // add tile to index
             else
@@ -102,9 +144,38 @@ public class Map : MonoBehaviour
                 if (m_tileMap[ix,iy] == null)
                 {
                     print("ERROR : tile [" + ix + "," + iy + "] is NULL");
+                    w_correctlyInitialized = false;
                 }
             }
         }
+
+        print("NOMBRE DE TUILES " + m_size);
+
+        return w_correctlyInitialized;
+    }
+
+    /// <summary>
+    /// Initialise Units based on existing units on the scene
+    /// Map initialising must have been called before
+    /// </summary>
+    ///  /// <returns>True if correctly init, false otherwise</returns>
+    private bool InitialiseUnits()
+    {
+        bool w_correctlyInitialized = true; // will be set to false when encoutering errors
+        // get all tiles in the scene
+        Unit[] w_units = FindObjectsOfType<Unit>();
+
+        foreach(Unit w_unit in w_units)
+        {
+            // adding unit to list
+            w_listOfUnit.Add(w_unit);
+            // setting unit position on the grid
+            w_unit.setGridPosition( convertToGridPosition(w_unit.transform.position) );
+
+            //w_unit.transform.Translate(Vector2.down * 2);
+        }
+
+        return w_correctlyInitialized;
     }
 
     /// <summary>
@@ -112,9 +183,60 @@ public class Map : MonoBehaviour
     /// </summary>
     /// <param name="ai_position">Position of the element (Z will be ignored)</param>
     /// <returns>2D index position</returns>
-    private Point convertPositionToIndexes(Vector3 ai_position)
+    private Point convertToGridPosition(Vector3 ai_position)
     {
-        return new Point( (int) (ai_position.x - m_positionToIndex.X), (int) (ai_position.y - m_positionToIndex.Y)  );
+        // check that TileSize value has correctly been set
+        if(TileSize <= 0)
+        {
+            throw new Exception("TileSize <= 0 : value makes no sense");
+        }
+        // half size of a tile
+        float w_halfTile = TileSize / 2;
+
+        // set the position in the Map frame
+        PointF w_IndexPositionF = new PointF(ai_position.x - m_positionToIndex.X, ai_position.y - m_positionToIndex.Y);
+        // position as trucated int value (ex 1.5 --> 1)
+        Point w_IndexPosition = new Point((int) Math.Truncate(w_IndexPositionF.X), (int) Math.Truncate(w_IndexPositionF.Y));
+
+        // check for each bound to determine if it has to be adjusted
+        // Manage Y axis
+        if ( (w_IndexPositionF.X - w_IndexPosition.X) < -w_halfTile) // left bound
+        {
+            w_IndexPosition.X -= 1;
+        }
+        else if ( ( w_IndexPositionF.X - w_IndexPosition.X) > w_halfTile) // right bound
+        {
+            w_IndexPosition.X += 1;
+        }
+        // Manage Y axis
+        if ( (w_IndexPositionF.Y - w_IndexPosition.Y) < -w_halfTile) // lower bound
+        {
+            w_IndexPosition.Y -= 1;
+        }
+        else if ( (w_IndexPositionF.Y - w_IndexPosition.Y) > w_halfTile) // upper bound
+        {
+            w_IndexPosition.Y += 1;
+        }
+
+        // check if computed point is in bounds
+        if (!isInBounds(w_IndexPosition))
+        {
+            print(ai_position + " -->" + w_IndexPosition);
+            throw new IndexOutOfRangeException("Computed Position OOR : " + w_IndexPosition);
+        }
+
+        // return result
+        return w_IndexPosition;
+    }
+
+    /// <summary>
+    /// Indicates if given indexes are in bound of the map
+    /// </summary>
+    /// <param name="ai_indexPosition">Index you want to check</param>
+    /// <returns>True if in bounds false otherwise</returns>
+    bool isInBounds(Point ai_indexPosition)
+    {
+        return (ai_indexPosition.X >= 0 && ai_indexPosition.Y >= 0 && ai_indexPosition.X < m_size.X && ai_indexPosition.Y < m_size.Y);
     }
     #endregion
 }
